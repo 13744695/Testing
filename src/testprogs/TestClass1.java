820887c11cd0d1df2961d53f549895a828a9aab3
13744695 <HelenaRothwell@gmail.com>
2015-06-11 13:19:51 +0100

    first commit

--- src/testprogs/TestClass1.java
+++ src/testprogs/TestClass1.java
@@ -3,6 +3,7 @@ package testprogs;
 public class TestClass1 {
 	public static void main(String []args){
 	System.out.println("tests testings and more testing tests");
+	System.out.println("welcome Dee, Derri and Mel");
 
 	}
 }
