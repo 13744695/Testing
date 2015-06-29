16ad8852ad85943ef10769bafecccb42564b7b90
13744695 <HelenaRothwell@gmail.com>
2015-06-29 19:19:43 +0100

    update

--- src/testprogs/TestClass1.java
+++ src/testprogs/TestClass1.java
@@ -2,8 +2,35 @@ package testprogs;
 
 public class TestClass1 {
 	public static void main(String []args){
+		
 	System.out.println("tests testings and more testing tests");
 	System.out.println("welcome Dee, Derri and Mel");
+	
+	Machine m1 = new Machine();
+	Machine m2 = new Machine();
+	
+	String name = "";
+	int num = 0;
 
 	}
+public void setInfo(String name, int num){
+	
+	
+	name = name;
+	num = num;
+}
+}
+
+
+
+
+
+
+
+
+class Machine{
+	
+	String name = "";
+	int num = 0;
+	System.out.println();
 }
